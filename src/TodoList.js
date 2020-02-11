63b1863563781a41fab06d7c2b92f206059cc2db
Guilherme <guiduck02@gmail.com>
2020-02-11 19:50:21 -0300

    finished simple todo aplication

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import Todo from './Todo';
 
-export default function TodoList({ todos }) {
-  return <div> {todos.length} </div>;
+export default function TodoList({ todos, toggleTodo }) {
+  return todos.map(todo => {
+    return <Todo key={todo.id} toggleTodo={toggleTodo} todo={todo} />;
+  });
 }
