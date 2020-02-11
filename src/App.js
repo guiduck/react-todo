c5a5687e73484a57738f932cfc8e3b5972db1fe4
Guilherme <guiduck02@gmail.com>
2020-02-10 23:26:02 -0300

    todo list set. Next map todos to show up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import TodoList from './TodoList';
 
 function App() {
+  const [todos, setTodo] = useState(['todo 1', 'todo 2']);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <TodoList todos={todos} />
+      <input type="text" />
+      <button> add </button>
+      <button> clear </button>
+      <div> 0 left to do </div>
+    </>
   );
 }
 
